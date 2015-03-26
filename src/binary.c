3983d4d1d363412b863aabd1219676c589d9ed41
Samuel Otter <samuel@otter.se>
2015-03-26 21:54:46 +0100

    Add ending null-byte to binaries

--- src/binary.c
+++ src/binary.c
@@ -7,18 +7,18 @@
 /* API ************************************************************************/
 
 Binary *binary_new(Scope *scope, size_t size) {
-  Binary *binary = scope_alloc(scope, sizeof(Binary) + size);
+  Binary *binary = scope_alloc(scope, sizeof(Binary) + size + 1);
   binary->size   = size;
   binary->header = ref_header(TYPEID_BINARY, 0);
-  memset(binary->data, 0, binary->size);
+  memset(binary->data, 0, binary->size + 1);
   return binary;
 }
 
 Binary *binary(Scope *scope, const uint8_t *data, size_t size) {
-  Binary *binary = scope_alloc(scope, sizeof(Binary) + size);
+  Binary *binary = scope_alloc(scope, sizeof(Binary) + size + 1);
   binary->size   = size;
   binary->header = ref_header(TYPEID_BINARY, 0);
-  memcpy(binary->data, data, size);
+  memcpy(binary->data, data, size + 1);
   return binary;
 }
 
